3ae52af8324c5488a8e662e9e32e2369f0772831
Daniel (TMP) Seuffer <authanram@gmail.com>
2020-07-05 17:32:40 +0200

    Change layouts view inheritance

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,17 +7,7 @@
 </head>
 <body>
 @include('layouts.partials.gtm-body')
-@include('cookieConsent::index')
-@include('layouts.partials.background', ['style' => true])
-<div id="app">
-    @include('layouts.partials.overlay')
-    <div class="page">
-        <div class="min-h-full relative z-0">
-            @include('layouts.partials.background', ['dom' => true])
-            @yield('app')
-        </div>
-    </div>
-</div>
+@yield('app')
 @include('layouts.partials.script')
 </body>
 </html>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,51 +2,69 @@
 
 @section('app')
 
-    <div class="h-16 shadow-lg sticky top-0 w-full z-10"></div>
+    <div id="app">
 
-    <x-card class="-mt-16 md:mb-0 sticky top-0 z-40 reject:margin padding rounded-lg shadow-md">
+        @include('cookieConsent::index')
 
-        @component('components.menu', ['border' => true])
+        @include('layouts.partials.background', ['style' => true])
 
-            @include('layouts.partials.logo')
+        @include('layouts.partials.overlay')
 
-        @endcomponent
-
-    </x-card>
-
-    <x-card
-        class="bg-opacity-95 md:border-b-2 md:border-gray-100 mb-5 md:mb-10 md:pb-1 pt-2 z-20 reject:bg-opacity-90 border border-gray-100 padding rounded-lg shadow-md"
-        even
-    >
-
-        @includeWhen($title ?? null, 'layouts.partials.page-title')
-
-    </x-card>
-
-    <x-container>
-
-        <div class="hidden md:block">
-
-            @include('layouts.partials.design-images')
-
+        <div class="page">
+            <div class="min-h-full relative z-0">
+                @include('layouts.partials.background', ['dom' => true])
+                @yield('app')
+            </div>
         </div>
 
-        <div class="relative">
 
-            @include('layouts.partials.content')
+        <div class="h-16 shadow-lg sticky top-0 w-full z-10"></div>
 
-        </div>
+        <x-card class="-mt-16 md:mb-0 sticky top-0 z-40 reject:margin padding rounded-lg shadow-md">
 
-    </x-container>
+            @component('components.menu', ['border' => true])
 
-    <x-container>
+                @include('layouts.partials.logo')
 
-        @include('layouts.partials.footer')
+            @endcomponent
 
-    </x-container>
+        </x-card>
 
-    <div class="h-8"></div>
+        <x-card
+            class="bg-opacity-95 md:border-b-2 md:border-gray-100 mb-5 md:mb-10 md:pb-1 pt-2 z-20 reject:bg-opacity-90 border border-gray-100 padding rounded-lg shadow-md"
+            even
+        >
 
-    @include('layouts.partials.made-with-love')
+            @includeWhen($title ?? null, 'layouts.partials.page-title')
+
+        </x-card>
+
+        <x-container>
+
+            <div class="hidden md:block">
+
+                @include('layouts.partials.design-images')
+
+            </div>
+
+            <div class="relative">
+
+                @include('layouts.partials.content')
+
+            </div>
+
+        </x-container>
+
+        <x-container>
+
+            @include('layouts.partials.footer')
+
+        </x-container>
+
+        <div class="h-8"></div>
+
+        @include('layouts.partials.made-with-love')
+
+    </div>
 
 @endsection
