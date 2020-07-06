eb0093c83d618aaa02fed5b80c99c6c685c6f14d
Daniel (TMP) Seuffer <authanram@gmail.com>
2020-07-06 08:29:41 +0200

    Update changelog

--- resources/views/changelog/2020-07-05/index.blade.php
+++ resources/views/changelog/2020-07-05/index.blade.php
@@ -3,6 +3,7 @@
     * __ESC__ will toggle the confirmation modal window <small>if changes has been done before.</small>
         * __Enter__ will dismiss the modal window <small>while in confirmation state.</small>
     * __Enter__ will submit the event attendance form <small>if changes has been done before and while not in confirmation state.</small>
+* Added ability to dismiss a modal window on outside click <small>if no changes has been done.</small>
 * Changed the orientation of the modal window <small>to "align-bottom" for mobile devices.</small>
 * Changed the orientation of the modal actions <small>to "justify-end" for mobile devices.</small>
 * Changed the sequence of the modal actions <small>for mobile devices.</small>
