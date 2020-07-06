3f82f2424a2ef1f2b91db4d9cfe0f3a2738bcc3e
Daniel (TMP) Seuffer <authanram@gmail.com>
2020-07-06 04:52:01 +0200

    Fix wrong changelog date

---
+++ resources/views/changelog/2020-07-05/index.blade.php
@@ -0,0 +1,11 @@
+* Added keyboard event listeners <small>for modals.</small>
+    * __ESC__ will now dismiss the modal window <small>if no changes has been done.</small>
+    * __ESC__ will toggle the confirmation modal window <small>if changes has been done before.</small>
+        * __Enter__ will dismiss the modal window <small>while in confirmation state.</small>
+    * __Enter__ will submit the event attendance form <small>if changes has been done before and while not in confirmation state.</small>
+* Changed the orientation of the modal window <small>to "align-bottom" for mobile devices.</small>
+* Changed the orientation of the modal actions <small>to "justify-end" for mobile devices.</small>
+* Changed the sequence of the modal actions <small>for mobile devices.</small>
+* Fix price position <small>for mobile devices.</small>
+![Modal orientation](/images/changelog/2020-07-05-mobile-button-orientation.png)
+<small>(Mobile View)</small>
