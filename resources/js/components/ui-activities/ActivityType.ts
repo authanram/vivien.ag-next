6286a15aa7160a04ae5d248d184c9d5973346f53
Daniel (TMP) Seuffer <authanram@gmail.com>
2021-01-03 18:14:07 +0100

    Fix html not being rendered

--- resources/js/components/ui-activities/ActivityType.ts
+++ resources/js/components/ui-activities/ActivityType.ts
@@ -16,21 +16,23 @@ export default {
     },
 
     methods: {
-        readTime (body: string): number {
+        readTime(body: string): number {
             const readTime = Math.round(body.split(' ').length / 200)
             return readTime > 0 ? readTime : 1
         },
 
-        truncate (body: string, length= 30, ending: string | null = '...') {
+        truncate(body: string, length = 30, ending: string | null = '...') {
             if (body.split(' ').length <= length) {
                 return body
             }
 
             const action = '<span class="font-medium group-hover:underline">Weiterlesen</span>'
 
-            return body.split(" ").splice(0, length).join(' ')
-                + (ending ? ` ${ending} ` : '')
-                + `<span class="text-gray-500 text-right text-sm">${action}</span>`
+            return (
+                body.split(' ').splice(0, length).join(' ') +
+                (ending ? ` ${ending} ` : '') +
+                `<span class="text-gray-500 text-right text-sm">${action}</span>`
+            )
         },
     },
 }
--- resources/js/components/ui-activities/UiActivityEvents.vue
+++ resources/js/components/ui-activities/UiActivityEvents.vue
@@ -9,9 +9,7 @@
                             {{ subject.created_at_readable }}
                         </span>
                     </time>
-                    <span class="block text-xs text-gray-500">
-                        1 Min. Lesedauer
-                    </span>
+                    <span class="block text-xs text-gray-500"> 1 Min. Lesedauer </span>
                 </span>
             </span>
             <template v-for="(tag, index) in subject.tags">
@@ -23,31 +21,18 @@
                     class="mb-2"
                 />
             </template>
-            <ripple
-                class="mt-4 text-gray-800"
-                nudge="5"
-                dark
-            >
+            <ripple class="mt-4 text-gray-800" nudge="5" dark>
                 <a
                     :class="`hover:text-${accent}-600`"
                     href="/seminare"
                     class="block leading-6 text-gray-600"
                 >
-                    <span
-                        class="font-semibold leading-tight text-xl"
-                        v-html="name"
-                    />
-                    <p class="font-medium mt-2 text-base">
-                        {{ truncate(subject.description) }}
-                    </p>
+                    <span class="font-semibold leading-tight text-xl" v-html="name" />
+                    <p class="mt-2 text-base" v-html="truncate(subject.description)" />
                     <p class="mb-1 mt-2">
                         <span class="font-normal">Datum</span>
-                        {{ dateFrom }},
-                        {{ `${subject.from.hours}:${subject.from.minutes}` }} -
-                        <span
-                            v-if="dateFrom !== dateTo"
-                            class="font-normal"
-                        >{{ dateTo }},</span>
+                        {{ dateFrom }}, {{ `${subject.from.hours}:${subject.from.minutes}` }} -
+                        <span v-if="dateFrom !== dateTo" class="font-normal">{{ dateTo }},</span>
                         {{ `${subject.to.hours}:${subject.to.minutes}` }}
                     </p>
                     <p class="mb-1">
@@ -58,7 +43,13 @@
                     </p>
                     <p class="mb-1">
                         <span class="font-normal">Preis</span>
-                        <span v-html="subject.price === 0 ? '<i class=\'text-gray-500\'>kostenlos</i>' : `${subject.price} &euro;`" />
+                        <span
+                            v-html="
+                                subject.price === 0
+                                    ? '<i class=\'text-gray-500\'>kostenlos</i>'
+                                    : `${subject.price} &euro;`
+                            "
+                        />
                     </p>
                 </a>
             </ripple>
@@ -67,28 +58,28 @@
 </template>
 
 <script lang="ts">
-    import ActivityType from './ActivityType'
-    import { Event } from '@/state/models'
+import ActivityType from './ActivityType'
+import { Event } from '@/state/models'
 
-    export default {
-        mixins: [ActivityType],
+export default {
+    mixins: [ActivityType],
 
-        computed: {
-            subject (): Event {
-                return this.actionable
-            },
-
-            dateFrom (): string {
-                return `${this.subject.from.day}.${this.subject.from.month}.${this.subject.from.year}`
-            },
-
-            dateTo (): string {
-                return `${this.subject.to.day}.${this.subject.to.month}.${this.subject.to.year}`
-            },
-
-            name (): string {
-                return this.subject.event_type.name.replace(/(\S+)-(\S+)/g,'$1&#8288;-&#8288;$2')
-            },
+    computed: {
+        subject(): Event {
+            return this.actionable
         },
-    }
+
+        dateFrom(): string {
+            return `${this.subject.from.day}.${this.subject.from.month}.${this.subject.from.year}`
+        },
+
+        dateTo(): string {
+            return `${this.subject.to.day}.${this.subject.to.month}.${this.subject.to.year}`
+        },
+
+        name(): string {
+            return this.subject.event_type.name.replace(/(\S+)-(\S+)/g, '$1&#8288;-&#8288;$2')
+        },
+    },
+}
 </script>
