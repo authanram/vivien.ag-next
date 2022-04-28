3e87516ff12566372cd1a4fa937e2bb89f2a9054
Daniel Seuffer <authanram@gmail.com>
2022-04-28 12:26:16 +0200

    WIP

--- app/Nova/Dashboards/Main.php
+++ app/Nova/Dashboards/Main.php
@@ -2,15 +2,27 @@
 
 namespace App\Nova\Dashboards;
 
-use Laravel\Nova\Cards\Help;
+use App\Nova\Cards\PageViewsMetric;
+use App\Nova\Cards\VisitorsMetric;
+use GijsG\SystemResources\SystemResources;
 use Laravel\Nova\Dashboards\Main as Dashboard;
+use Tightenco\NovaGoogleAnalytics\MostVisitedPagesCard;
 
 class Main extends Dashboard
 {
+    public function name(): string
+    {
+        return 'Dashboard';
+    }
+
     public function cards(): array
     {
         return [
-            new Help,
+            new PageViewsMetric(),
+            new VisitorsMetric(),
+            new MostVisitedPagesCard(),
+            new SystemResources('ram'),
+            new SystemResources('cpu'),
         ];
     }
 }
