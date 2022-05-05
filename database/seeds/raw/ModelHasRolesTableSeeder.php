04fcf6408e733d18bf12edc9b857ad7585c0d2c6
Daniel Seuffer <authanram@gmail.com>
2022-05-05 04:51:03 +0200

    WIP

--- database/seeds/raw/ModelHasRolesTableSeeder.php
+++ database/seeds/raw/ModelHasRolesTableSeeder.php
@@ -1,3 +1,19 @@
 <?php
 
-return [];
+return [
+    [
+        'role_id' => 1,
+        'model_type' => 'App\\Models\\User',
+        'model_id' => 1,
+    ],
+    [
+        'role_id' => 2,
+        'model_type' => 'App\\Models\\User',
+        'model_id' => 2,
+    ],
+    [
+        'role_id' => 3,
+        'model_type' => 'App\\Models\\User',
+        'model_id' => 3,
+    ],
+];
--- database/seeds/raw/RolesTableSeeder.php
+++ database/seeds/raw/RolesTableSeeder.php
@@ -1,3 +1,25 @@
 <?php
 
-return [];
+return [
+    [
+        'id' => 1,
+        'name' => 'administrator',
+        'guard_name' => 'web',
+        'created_at' => '2020-10-25 10:36:24',
+        'updated_at' => '2020-10-25 10:36:24',
+    ],
+    [
+        'id' => 2,
+        'name' => 'moderator',
+        'guard_name' => 'web',
+        'created_at' => '2020-10-25 10:36:24',
+        'updated_at' => '2020-10-25 10:36:24',
+    ],
+    [
+        'id' => 3,
+        'name' => 'attendee',
+        'guard_name' => 'web',
+        'created_at' => '2020-10-25 10:36:24',
+        'updated_at' => '2020-10-25 10:36:24',
+    ],
+];
