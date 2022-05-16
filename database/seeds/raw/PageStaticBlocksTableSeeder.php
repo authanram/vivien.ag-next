b9835ffb63529fbdc58be4c3520128c8a0db51c4
Daniel Seuffer <authanram@gmail.com>
2022-05-16 10:18:16 +0200

    WIP

--- database/seeds/raw/PageStaticBlocksTableSeeder.php
+++ database/seeds/raw/PageStaticBlocksTableSeeder.php
@@ -4,7 +4,16 @@ return [
     [
         'id' => 1,
         'page_id' => 1,
-        'static_block_id' => 1,
+        'static_block_id' => 14,
+        'section' => 'content',
+        'created_at' => '2022-05-13 23:33:39',
+        'updated_at' => '2022-05-13 23:33:39',
+        'deleted_at' => null,
+    ],
+    [
+        'id' => 2,
+        'page_id' => 1,
+        'static_block_id' => 15,
         'section' => 'content',
         'created_at' => '2022-05-13 23:33:39',
         'updated_at' => '2022-05-13 23:33:39',
